da35baf0d9bb3dc5725ab07fdb2938ab26eec6ef
lubnakanan <150757995+lubnakanan@users.noreply.github.com>
2024-11-30 22:40:54 +0300

    ]weather_data.py

---
+++ weather_data.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+
+# API Configuration
+API_KEY = 'ae8948c438603b869d7ff9350deb65e3'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+CITY = 'London'
+
+def fetch_weather_data():
+    params = {
+        'q': CITY,
+        'appid': API_KEY,
+        'units': 'metric'
+    }
+    response = requests.get(BASE_URL, params=params)
+    return response.json()
+
+def extract_weather_data():
+    data = fetch_weather_data()
+    # Extract relevant data fields
+    weather_data = {
+        'city': CITY,
+        'temperature': data['main']['temp'],
+        'humidity': data['main']['humidity'],
+        'pressure': data['main']['pressure'],
+        'weather': data['weather'][0]['description'],
+        'timestamp': pd.to_datetime('now')
+    }
+    return weather_data
+
+# Fetch data
+weather = extract_weather_data()
+weather_df = pd.DataFrame([weather])
+
+# Save the data to a CSV (this can be part of the ETL pipeline)
+weather_df.to_csv('weather_data.csv', index=False)
